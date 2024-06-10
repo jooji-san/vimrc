3068320b91ddaec7ff1751d45c0a6bd8950a64ee
giorgi shengelaia <g.shengelaia2001@gmail.com>
2024-06-10 20:13:54 +0400

    10 july

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.background = "light"
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.g.autoformat = false
